e76b182e144e3230d940b92925a61b0fc60ae0c6
kasam75 <wjdghltjr22@naver.com>
2025-07-28 10:54:54 +0900

    2

--- src/main/java/Calculator2.java
+++ src/main/java/Calculator2.java
@@ -2,41 +2,44 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Calculator2 {
-        private final List<Double> resultList = new ArrayList<>();
+    private final List<Double> resultList = new ArrayList<>();
 
-        public double calculate ( int firstNumber, int secondNumber, char operator){
-            double result;
-            switch (operator) {
-                case '+':
-                    result = firstNumber + secondNumber;
-                    break;
-                case '-':
-                    result = firstNumber - secondNumber;
-                    break;
-                case '*':
-                    result = firstNumber * secondNumber;
-                    break;
-                case '/':
-                    if (secondNumber == 0) {
-                        throw new ArithmeticException("나눗셈 연산에서 분모(두 번째 정수)가 0일수 없습니다.");
-                        }
+    public double calculate(int firstNumber, int secondNumber, char operator) {
+        double result;
+        switch (operator) {
+            case '+':
+                result = firstNumber + secondNumber;
+                break;
+            case '-':
+                result = firstNumber - secondNumber;
+                break;
+            case '*':
+                result = firstNumber * secondNumber;
+                break;
+            case '/':
+                if (secondNumber == 0) {
+                    throw new ArithmeticException("나눗셈 연산에서 분모(두 번째 정수)가 0일수 없습니다.");
+                }
                 result = (double) firstNumber / secondNumber;
                 break;
-                default:
-                    throw new IllegalArgumentException("사칙연산 기호는 +,-,*,/ 중 하나여야 합니다.");
-            }
-            addResult(result);
-            return result;
+            default:
+                throw new IllegalArgumentException("사칙연산 기호는 +,-,*,/ 중 하나여야 합니다.");
         }
-        public List<Double> getResultList () {
-            return resultList;
-        }
-        public void addResult(double result){
-            resultList.add(result);
-        }
-        public void removeResult(){
-            resultList.remove(0);
-        }
+        addResult(result);
+        return result;
     }
+
+    public List<Double> getResultList() {
+        return resultList;
+    }
+
+    public void addResult(double result) {
+        resultList.add(result);
+    }
+
+    public void removeResult() {
+        resultList.remove(0);
+    }
+}
 
 
